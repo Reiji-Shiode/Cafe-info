9dc4f081463d57d6a6716edd717f72eb0f5e41d8
Reiji-Shiode <rerere0606@gmail.com>
2024-05-06 22:00:49 +0900

    プロジェクトの作成。README.md1no
     a

---
+++ README.md
@@ -0,0 +1,3 @@
+# 第8回課題
+
+この課題ではSpring BootとMySQLを用いたRead処理をするAPIを実装します。
---
+++ src/main/java/com/example/cafeinformation/CafeinformationApplication.java
@@ -0,0 +1,13 @@
+package com.example.cafeinformation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CafeinformationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CafeinformationApplication.class, args);
+	}
+
+}
